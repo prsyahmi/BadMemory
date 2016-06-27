1ef90ab4fb59e96c8399b79e60e5c924e3516fb8
Syahmi Azhar <syahmixp@gmail.com>
2016-06-27 13:00:17 +0800

    Added GPLv3 license.

--- BadMemoryManager/BadMemoryManager.cpp
+++ BadMemoryManager/BadMemoryManager.cpp
@@ -1,5 +1,21 @@
-// BadMemoryManager.cpp : Defines the entry point for the application.
-//
+/*
+	Manage bad memory regions for the BadMemory driver
+	Copyright (C) 2016  Syahmi Azhar
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include "stdafx.h"
 #include "BadMemoryManager.h"
@@ -337,7 +353,7 @@ void AddEntry()
 
 void DelEntry()
 {
-	int nSel = SendMessage(ListBoxHwnd, LB_GETCURSEL, 0, 0);
+	LRESULT nSel = SendMessage(ListBoxHwnd, LB_GETCURSEL, 0, 0);
 	if (nSel == LB_ERR) return;
 
 	BadRegions.erase(BadRegions.begin() + nSel);
